f9cc440f54deb3fbebb47c755cb0170f5085f0d2
alirast <xdabnormal@gmail.com>
2023-07-08 16:51:30 +0300

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by N S on 08.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
